76705364bce7a2ae4cd24d163c7c609b63cdb022
KerinaBakarudin <112407523+KerinaBakarudin@users.noreply.github.com>
2024-06-06 06:58:52 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
     <script src="https://kit.fontawesome.com/97216fb713.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Home</title>
 
     <style>
         body{
